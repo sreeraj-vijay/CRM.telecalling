d25aea784d58809ccb6de7ca5fbdbb20de893bd8
ABHIDAS-S <abhidasabhi1234@gmail.com>
2024-09-23 15:43:17 +0530

    port changed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true,
+  baseURL: "http://localhost:9000/api",
+  withCredentials: true
   // baseURL:"https://www.erp.camet.in/"
 })
 export default api
